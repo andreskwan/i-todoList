3a8fe2ed770118184f3206ac6061025d83d18be6
ANDRES KWAN <andreskwan@gmail.com>
2015-08-05 06:40:03 -0500

    where ViewController code will be put on

---
+++ ToDoApp/FetchedResultsControllerDataSource.h
@@ -0,0 +1,13 @@
+//
+//  FetchedResultsControllerDataSource.h
+//  ToDoApp
+//
+//  Created by Andres Kwan on 8/5/15.
+//  Copyright (c) 2015 Kwan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FetchedResultsControllerDataSource : NSObject
+
+@end
---
+++ ToDoApp/FetchedResultsControllerDataSource.m
@@ -0,0 +1,13 @@
+//
+//  FetchedResultsControllerDataSource.m
+//  ToDoApp
+//
+//  Created by Andres Kwan on 8/5/15.
+//  Copyright (c) 2015 Kwan. All rights reserved.
+//
+
+#import "FetchedResultsControllerDataSource.h"
+
+@implementation FetchedResultsControllerDataSource
+
+@end
